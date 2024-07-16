f6d6193598a9141a696def16ac9721f0523db22d
tapaderuza <tuemail@example.com>
2024-07-16 11:16:29 +0200

    Proyecto de GeoRadares

---
+++ src/main/java/com/example/GEORADARES/GeoradaresProyectoApplication.java
@@ -0,0 +1,13 @@
+package com.example.GEORADARES;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoradaresProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoradaresProyectoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GEORADARES/GeoradaresProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GEORADARES;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoradaresProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
